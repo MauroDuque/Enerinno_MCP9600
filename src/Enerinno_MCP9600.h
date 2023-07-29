188310eac82ce6b6ff2d8f3b84bb63a784b0b7f5
MauroDuque <mauricio.duque.eje@gmail.com>
2023-07-29 18:10:36 -0400

    fix 3

--- src/Enerinno_MCP9600.h
+++ src/Enerinno_MCP9600.h
@@ -1,6 +1,8 @@
 #ifndef ENERINNO_MCP9600_H
 #define ENERINNO_MCP9600_H
 
+#include <SparkFun_MCP9600.h>
+
 void init_thermocouples();
 float get_sensor_value(int sensorIndex);
 float getSensorTemperature(int sensorIndex);
